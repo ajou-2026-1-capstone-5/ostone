a8115ffa41654c855efb8800db6ebb889a4119af
배성연 <bsy309@ajou.ac.kr>
2026-05-10 00:38:01 +0900

    fix(domain-pack): classify clone failure as conflict

--- backend/src/main/java/com/init/domainpack/application/exception/DomainPackVersionCloneFailedException.java
+++ backend/src/main/java/com/init/domainpack/application/exception/DomainPackVersionCloneFailedException.java
@@ -1,10 +1,11 @@
 package com.init.domainpack.application.exception;
 
-import com.init.shared.application.exception.BadRequestException;
+import com.init.shared.application.exception.DuplicateException;
 
-public class DomainPackVersionCloneFailedException extends BadRequestException {
+public class DomainPackVersionCloneFailedException extends DuplicateException {
 
   public DomainPackVersionCloneFailedException(Long packId, Throwable cause) {
-    super("DOMAIN_PACK_VERSION_CLONE_FAILED", "도메인 팩 버전 복제에 실패했습니다. packId=" + packId, cause);
+    super("DOMAIN_PACK_VERSION_CLONE_FAILED", "도메인 팩 버전 복제에 실패했습니다. packId=" + packId);
+    initCause(cause);
   }
 }
